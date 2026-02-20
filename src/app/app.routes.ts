66beb0c03f2adb6ab7311e6a431146ce3f2d44b7
Haon607 <noah0676@gmail.com>
2026-02-20 23:05:39 +0100

    t

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from "./home/home";
+import { T } from "./t/t";
 
 export const routes: Routes = [
-    {path: '', component: Home}
+    {path: '', component: Home},
+    {path: '/ttt', component: T}
 ];
---
+++ src/app/t/t.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-t',
+  imports: [],
+  templateUrl: './t.html',
+  styleUrl: './t.css',
+  standalone: true
+})
+export class T {
+
+}
